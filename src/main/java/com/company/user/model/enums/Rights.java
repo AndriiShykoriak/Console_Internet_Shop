81cf6c5aa109edde9ab37bcb90dc777028f15188
AndriiShykoriak <andrii.shykoriak@gmail.com>
2020-08-01 06:29:33 +0300

    hepling enums to user Rights,Status

---
+++ src/main/java/com/company/user/model/enums/Rights.java
@@ -0,0 +1,6 @@
+package com.company.user.model.enums;
+
+public enum  Rights {
+    USER,
+    ADMIN
+}
---
+++ src/main/java/com/company/user/model/enums/Status.java
@@ -0,0 +1,6 @@
+package com.company.user.model.enums;
+
+public enum  Status {
+    ACTIVE,
+    BLOCKED
+}
