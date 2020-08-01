6ca1d0adaacc0961825296d6130f5a68a7267a9b
AndriiShykoriak <andrii.shykoriak@gmail.com>
2020-08-01 06:34:39 +0300

    main.class

---
+++ src/main/java/com/company/Main.java
@@ -0,0 +1,9 @@
+package com.company;
+
+import com.company.view.impl.AuthorizationMenuImpl;
+
+public class Main {
+    public static void main(String[] args) {
+        new AuthorizationMenuImpl().authorization();
+    }
+}
