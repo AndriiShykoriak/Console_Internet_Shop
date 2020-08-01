b4b874a51913d9e9e38aace3f3e959bfbd885135
AndriiShykoriak <andrii.shykoriak@gmail.com>
2020-08-01 06:25:20 +0300

    enum category products

---
+++ src/main/java/com/company/product/model/enums/Category.java
@@ -0,0 +1,15 @@
+package com.company.product.model.enums;
+
+public enum  Category {
+    MOBILE,
+    TABLETS,
+    LAPTOPS,
+    COMPUTER_PERIPHERAL,
+    MOBILE_ACCESSORY,
+    TABLET_ACCESSORY,
+    TELEVISION,
+    AUDIO_VIDEO,
+    CAMERA,
+    GAMING,
+    OTHER
+}
